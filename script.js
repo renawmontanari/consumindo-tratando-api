dd31e8e3a6880744fe322af10ebf35cae73a36f8
renawmontanari <regiangelica@gmail.com>
2024-05-01 08:35:29 -0300

    async-funcao

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-const consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
-    .then(resposta => resposta.json()
-    .then(r =>  {
-        if (r.erro) {
-            throw Error('Esse CEP não existe!')
-        } else
-            console.log(r)})
-    .catch(erro => console.log(erro))
-    .finally(mensagem => console.log('Processamento concluído!'))
-);
 
-console.log(consultaCep);
+async function buscaEndereco() {
+    const consultaCep = await fetch('https://viacep.com.br/ws/01001000/json/');
+    const resposta = await consultaCep.json();
+    console.log(resposta)
+}
+
+buscaEndereco();
